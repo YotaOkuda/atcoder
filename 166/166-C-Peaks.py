2794eda4e01b4e22c46c73d89a36f3de2baeff90
YotaOkuda <yotaai200@gmail.com>
2024-07-23 10:32:24 +0900

    166

--- 166/166-C-Peaks.py
+++ 166/166-C-Peaks.py
@@ -14,15 +14,17 @@ for i in range(M):
 
 ans = 0
 
+# 各頂点とそれに隣接する頂点の高さを比べる
 for i in range(N):
     flag = True
-    for j in G[i]:
-        if H[i] <= H[j]:
-            flag = False
+    for j in G[i]:      # 隣接する頂点の探索
+        if H[i] <= H[j]:    # 現在の頂点の高さH[i]と隣接する頂点の高さH[j]を比べる
+            flag = False    # 隣接する頂点のほうが高ければFalseにする
         
-    if flag:
+    if flag:        # 現在の頂点が隣接する頂点の中で一番高いとき
         ans += 1
     
 print(ans)
+
 
 # 計算量 O(N+ M) < 10^8
