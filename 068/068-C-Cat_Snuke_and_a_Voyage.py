72260d481d2dfe7b63d7d7584c7173c3494a16b6
YotaOkuda <yotaai200@gmail.com>
2024-07-23 11:08:41 +0900

    068

--- 068/068-C-Cat_Snuke_and_a_Voyage.py
+++ 068/068-C-Cat_Snuke_and_a_Voyage.py
@@ -11,8 +11,9 @@ for i in range(M):
     G[a].append(b)
     G[b].append(a)
 
+# 島１を探索
 for i in G[0]:
-    ans = (N - 1) in G[i]
+    ans = (N - 1) in G[i]   # 島１に隣接する島から島Nに行けるか（２つの便でゴールできるか）
     if ans:
         break
 
