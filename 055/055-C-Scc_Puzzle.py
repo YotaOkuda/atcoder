49caace173614b868045dae458b802be877ead8d
YotaOkuda <yotaai200@gmail.com>
2024-09-23 15:20:29 +0900

    055

--- 055/055-C-Scc_Puzzle.py
+++ 055/055-C-Scc_Puzzle.py
@@ -4,7 +4,7 @@ N, M = map(int, input().split())
 count = 0
 if M - N*2 >= 0:  # (S=1,C=2)で作成して、Cに余りが出る場合
     count += N
-    M -= N * 2  # Cの残り枚数を更新
+    M -= N * 2  # Cの残りの個数を更新
     count += M // 4  # (C=4)で作れる数を求める
 elif M - N*2 < 0:  # Cが足りなくて(S=1,C=2)でしか作成できない場合
     count += M // 2
