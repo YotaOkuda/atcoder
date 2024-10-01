7fa5fa6e5df49a68869087bffab4f3888127aff1
YotaOkuda <yotaai200@gmail.com>
2024-10-01 11:12:38 +0900

    6.2.13.own.py

---
+++ 参考書/6.2.13.own.py
@@ -0,0 +1,20 @@
+N, W = map(int, input().split())
+w = list(map(int, input().split()))
+v = list(map(int, input().split()))
+
+def rec(n, sw, sv):
+    if n == N:
+        return sv
+    
+    result = 0
+    
+    score = rec(n + 1, sw, sv)
+    result = max(result, score)
+    
+    if sw + w[n] <= W:
+        score = rec(n + 1, sw + w[n], sv + v[n])
+        result = max(result, score)
+    
+    return result
+
+print(rec(0, 0, 0))
