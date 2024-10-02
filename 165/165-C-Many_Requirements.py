52d4d7cd44587a0cee95edbb032c1e22ab1b2d1a
YotaOkuda <yotaai200@gmail.com>
2024-10-02 11:20:06 +0900

    A, B, C, D

--- 165/165-C-Many_Requirements.py
+++ 165/165-C-Many_Requirements.py
@@ -1,24 +1,3 @@
-'''N, M, Q = map(int, input().split())
-
-a = [0] * Q
-b = [0] * Q
-c = [0] * Q
-d = [0] * Q
-for i in range(Q):
-    #上から順番に代入していく
-    a[i], b[i], c[i], d[i] = map(int, input().split())
-
-def rec(A):
-    if len(A) == N:
-        print(A)
-        return
-    
-    for v in range(1, M):
-        A.append(v)
-        rec(A)
-        A.pop()
-        
-rec([])'''
 N, M, Q = map(int, input().split())
 
 a = [0] * Q
@@ -29,6 +8,24 @@ for i in range(Q):
     #上から順番に代入していく
     a[i], b[i], c[i], d[i] = map(int, input().split())
 
+result = 0
+def rec(A):
+    if len(A) == N:
+        for i in range(len(A)):
+            if A[i] > A[i + 1]:
+                break
+            else:
+                count = calc(A)
+                result = (result, count)
+        print(result)
+        return
+    
+    for v in range(1, M + 1):
+        A.append(v)
+        rec(A)
+        A.pop()
+        
+
 def calc(A):
     sum = 0
     for i in range(Q):
@@ -37,5 +34,4 @@ def calc(A):
     
     return sum
 
-A = [1, 3, 4]
-print(calc(A))
+rec([])
