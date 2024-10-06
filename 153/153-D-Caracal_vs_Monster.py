6bfb3a281f9f2a93aaa9fa3a84194327cae01bba
YotaOkuda <yotaai200@gmail.com>
2024-10-06 21:58:09 +0900

    A, D

--- 153/153-D-Caracal_vs_Monster.py
+++ 153/153-D-Caracal_vs_Monster.py
@@ -1,8 +1,35 @@
+H = int(input())
+
+def rec(H, N):
+    if H == 0:
+        return N - 1
+        
+    return rec(H // 2, N * 2)
+    
+print(rec(H, 1))
+
+
+# 計算量 O(logH)
+
+
+# 別解１
+H = int(input())
 def rec(H):
     if H == 1:
-        ans += 1
-        
-    ans = 0
-    rec(H//2)
-    ans += 2
+        return 1
     
+    # 2 体に分裂するのに「+1」、N//2 を 2体倒すのに rec(N//2)*2
+    return rec(H // 2)*2 + 1
+print(rec(H))
+
+
+
+# 別解２
+H = int(input())
+
+ans = 0
+while H > 0:
+    ans = ans + (ans + 1)
+    H = H // 2
+    
+print(ans)
