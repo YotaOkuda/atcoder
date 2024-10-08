4eebacd3a14ca0fd2e446c1522f82bfec6f99ef8
YotaOkuda <yotaai200@gmail.com>
2024-10-08 18:45:50 +0900

    A, C

--- 173/173-C-H_and_V.py
+++ 173/173-C-H_and_V.py
@@ -7,27 +7,36 @@ for _ in range(H):
 
 ans = 0
 
+# 行と列の組み合わせをビット全探索
 for num_H in range(1 << H):
     for num_W in range(1 << W):
+        
         # Cを書き換えるためにコピー
         tmp_C = copy.deepcopy(C) 
         
-        # 行を選択するかどうか
+        # 行を選択するかどうか判断
         for shift_H in range(H):
+            # 行を選択するなら
             if 1 & (num_H >> shift_H) == 1:
-                # print(f'shift_H:{shift_H}')
+                # 行を赤に塗り替える
                 for w in range(W):
-                    tmp_C[shift_H][w] = "x"  # 行を黒に塗り替える
+                    tmp_C[shift_H][w] = "x"
                 
-        # 列を選択するかどうか
+        # 列を選択するか判断
         for shift_W in range(W):
+            # 列を選択するなら
             if 1 & (num_W >> shift_W) == 1:
-                # print(f'shift_W:{shift_W}')
+                # 列を赤に塗り替える
                 for h in range(H):
                     tmp_C[h][shift_W] = "x"
         
+        # 黒の数をカウント
         count = sum([row.count('#') for row in tmp_C])                    
+        
         if count == K:
             ans += 1
 
 print(ans)
+
+
+# 計算量 O(2^(H + W) * H * W) < 147,456 < 10^8
