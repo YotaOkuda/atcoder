2ffe1672f44975777380fb5476ee5b1e1578d340
YotaOkuda <yotaai200@gmail.com>
2024-11-10 20:40:42 +0900

    A, B, C

--- 379/379-C-Sowing_Stones.py
+++ 379/379-C-Sowing_Stones.py
@@ -2,7 +2,12 @@
 <方針>
 - X[i] にあるコマ A[i] で、X[i + 1] までの空きマスを埋める
 - 余ったコマは X[i + 1] まで移動させ、A[i + 1] に加算する
-- 空きマスを埋められるコマがなければ処理を終了
+- 空きマスを埋められるコマがなければ不可能と判断し、処理を終了
+- コマの移動回数は X[i] ごとに求める、O(1) で計算する
+  sum(range(1, distance + 1)) だと TLE になる
+
+<参考にした考え方>
+- X を基準としてソートする（あらかじめソート済みだと思っていた）
 '''
 
 import sys
@@ -11,10 +16,12 @@ N, M = map(int, input().split())
 X = list(map(int, input().split()))
 A = list(map(int, input().split()))
 
+# X を基準としてソートする
 sorted_pairs = sorted(zip(X, A))
 X, A = map(list, zip(*sorted_pairs))
 
-if 1 not in X or sum(A) != N or N - X[-1] + 1 < A[-1]:
+# 最初のマスにコマがない、マスの数とコマ数が一致しないなど不可能な場合
+if 1 not in X or sum(A) != N:
     print(-1)
     sys.exit()
 
@@ -22,6 +29,7 @@ if 1 not in X or sum(A) != N or N - X[-1] + 1 < A[-1]:
 count = 0
 # X[i + 1] の場所に持ち越しのコマ
 next_koma = 0
+# 可能か不可能か判断するフラグ
 flag = True
 
 i = 0
@@ -29,7 +37,7 @@ while i < M:
     # コマが置いてある最後尾のマスの場合
     # 残りのマスを埋めるだけ
     if i == M - 1:
-        count += sum(range(1, N - X[i] + 1))
+        count += (N - X[i] + 1) * (N - X[i]) // 2
         break
     
     # 空きマス
@@ -46,17 +54,20 @@ while i < M:
     # count += sum(range(1, distance + 1))
     
     # 余ったコマは、X[i + 1] マスに持ち越し
-    next_koma = X[i] - distance - 1
+    next_koma = A[i] - distance - 1
     # 余ったコマをがあれば
     if next_koma > 0:
         # X[i + 1] まで移動させるのに必要な操作回数を加算
         count += (distance + 1) * next_koma
-        
+   
     # i を更新
     i += 1
     
+
 # 答えを出力
 if flag:
     print(count)
 else:
     print(-1)
+    
+# 計算量 O(M log M), 最初のソートが一番かかる, while の処理は O(M)
