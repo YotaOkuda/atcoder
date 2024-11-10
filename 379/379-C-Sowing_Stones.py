9a1aaef2ef45c751e3955be126635592089fe2ff
YotaOkuda <yotaai200@gmail.com>
2024-11-10 15:19:08 +0900

    A, B, C(process of solving)

--- 379/379-C-Sowing_Stones.py
+++ 379/379-C-Sowing_Stones.py
@@ -1,14 +1,22 @@
+'''
+<方針>
+- X[i] にあるコマ A[i] で、X[i + 1] までの空きマスを埋める
+- 余ったコマは X[i + 1] まで移動させ、A[i + 1] に加算する
+- 空きマスを埋められるコマがなければ処理を終了
+'''
+
 import sys
 
 N, M = map(int, input().split())
 X = list(map(int, input().split()))
 A = list(map(int, input().split()))
 
+sorted_pairs = sorted(zip(X, A))
+X, A = map(list, zip(*sorted_pairs))
 
 if 1 not in X or sum(A) != N or N - X[-1] + 1 < A[-1]:
     print(-1)
     sys.exit()
-
 
 # 操作回数
 count = 0
@@ -18,6 +26,8 @@ flag = True
 
 i = 0
 while i < M:
+    # コマが置いてある最後尾のマスの場合
+    # 残りのマスを埋めるだけ
     if i == M - 1:
         count += sum(range(1, N - X[i] + 1))
         break
@@ -32,7 +42,8 @@ while i < M:
         break
     
     # 空きマスを埋めるのに必要な操作回数を加算
-    count += sum(range(1, distance + 1))
+    count += distance * (distance + 1) // 2
+    # count += sum(range(1, distance + 1))
     
     # 余ったコマは、X[i + 1] マスに持ち越し
     next_koma = X[i] - distance - 1
