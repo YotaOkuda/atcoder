c2b0c901313a740ab794f0c7cac1c3743e8bf95f
YotaOkuda <yotaai200@gmail.com>
2024-11-11 19:01:18 +0900

    A

---
+++ abc127/a/main.py
@@ -0,0 +1,12 @@
+a, b = map(int, input().split())
+if a >= 13:
+    print(b)
+elif a <= 5:
+    print(0)
+else:
+    print(int(b/2))
+
+'''elif 6 <= a <= 12:
+    print(int(b/2))
+else:
+    print(0)'''
