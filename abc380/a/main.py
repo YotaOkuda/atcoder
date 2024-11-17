f6fe13261ede774f78caf2cb46ae01322ce2923b
YotaOkuda <yotaai200@gmail.com>
2024-11-17 12:18:54 +0900

    A, B, C

--- abc380/a/main.py
+++ abc380/a/main.py
@@ -1,10 +1,20 @@
+'''
+<方針>
+- 入力をリストで受け取り、「'1', '2', '3'」それぞれカウントする
+- 条件を満たしているならば 'Yes' を出力、満たしていなければ 'No' を出力
+'''
+
 N = list(input())
 
+# カウント
 count_1 = N.count('1')
 count_2 = N.count('2')
 count_3 = N.count('3')
 
+# 条件を満たしているか
 if count_1 == 1 and count_2 == 2 and count_3 == 3:
     print('Yes')
 else:
     print('No')
+    
+# 計算量 O(1)
--- abc380/c/main.py
+++ abc380/c/main.py
@@ -1,14 +1,23 @@
-from collections import defaultdict
+'''
+<方針>
+- 0 の連続と、1 の連続を区間分割する
+- 1 の塊とその始点、終点（厳密には 1 が終わり、0 が始まるインデックス）、をリストに記録していく
+- リストの K（index : K - 1) 番目と K - 1（index : K - 2）の
+  「始点、終点、1 の塊」を参照し、文字列を組み替える
+'''
 
 N, K = map(int, input().split())
 S = str(input())
 
+# 1 の塊を記録するリスト
 oneBlock = []
 
+# 文字列を先頭から見て区間分割する
 i = 0
 while i < len(S):
     j = i
     
+    # 1 の塊なら、リストに「始点、終点、1 の塊」を記録
     if S[j] == '1':
         while j < len(S) and S[j] == '1':
             j += 1
@@ -19,10 +28,16 @@ while i < len(S):
     
     i = j
     
-
+# K 番目の「始点、終点、1 の塊」を参照
 kStart, kEnd, kOne = oneBlock[K - 1]
+# K 番目の 1 の塊を消した文字列を作成
 S = S[:kStart] + S[kEnd:]
 
+# K - 1 番目の「始点、終点、1 の塊」を参照
 k1Start, k1End, k1One = oneBlock[K - 2]
+# K 番目の 1 の塊を、K - 1 番目の 1 の塊の後ろにつける
 S = S[:k1End] + kOne + S[k1End:]
+
 print(S)
+
+# 計算量 O(N)
