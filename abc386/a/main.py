4af368807902c804f7f3bb6402f7b73e1b2eeb0e
YotaOkuda <yotaai200@gmail.com>
2024-12-29 10:47:53 +0900

    A, B, C

--- abc386/a/main.py
+++ abc386/a/main.py
@@ -1,18 +1,32 @@
-#A, B, C, D = map(int, input().split())
+'''
+<方針>
+- 1 枚加えて、フルハウスになる 4 枚の状況は以下の 2 通り
+  - (1) 2 枚ずつ同じ数字
+  - (2) 3 枚同じ数字
+- 上記の状況に対応できるように、1 - 14 の数字をカウントする
+  - (1) 2 枚同じ数字が 2 回出現するか
+  - (2) 3 枚同じ数字が 1 回出現するか
+'''
 card = list(map(int, input().split()))
 
+# 2 枚同じカードがある場合にカウントする
 two_count = 0
+# 3 枚同じカードがある場合にカウントする
 three_count = 0
+
+# 1 - 14 についてそれぞれカウントしていく
 for i in range(1, 14):
     count = card.count(i)
+    # 2 つ同じ数字をカウント
     if count == 2:
         two_count += 1
+    # 3 つ同じ数字をカウント
     elif count == 3:
         three_count += 1
 
-if two_count == 2:
-    print('Yes')
-elif three_count == 1:
+if two_count == 2 or three_count == 1:
     print('Yes')
 else:
     print('No')
+    
+# 計算量 O(1)
--- abc386/c/main.py
+++ abc386/c/main.py
@@ -1,17 +1,67 @@
+'''
+<方針>
+- 不可能な場合を考える
+  - (1) 文字列長の差が 1 より大きい場合（操作は 1 回のみ、1 回の操作で変更できる文字は 1 文字までのため）
+  - (2) 文字列長が同じで、違う文字が 2 つ以上
+  - (3) 文字列長の差が 1 で、違う文字が出てきた場合に文字列長の長い方の文字を消す回数が 2 回以上
+'''
+
 import sys
 
 K = int(input())
 S = list(str(input()))
 T = list(str(input()))
 
+# 違う文字の出現回数をカウントする
 count_diff = 0
+i, j = 0, 0
+
+# (1) の条件
 if abs(len(S) - len(T)) > 1:
     print('No')
+    sys.exit()
+    
+# (2) の条件
+if len(S) == len(T):
+    for i in range(len(S)):
+        if S[i] != T[i]:
+            count_diff += 1
+        if count_diff >= 2:
+            print('No')
+            sys.exit()
+    print('Yes')
+# (3) の条件
+else:
+    while i < len(S) and j < len(T):
+        if S[i] != T[j]:
+            count_diff += 1
+            if count_diff >= 2:
+                print('No')
+                sys.exit()
+            
+            if len(S) < len(T):
+                j += 1
+            else:
+                i += 1
+        else:
+            j += 1
+            i += 1
+    print('Yes')
+
+# 計算量 O(min(len(S), len(T))
+
+
+'''
+# (1) の条件
+if abs(len(S) - len(T)) > 1:
+    print('No')
+# (2) の条件
 elif len(S) == len(T):
     for i in range(len(S)):
         if S[i] != T[i]:
             count_diff += 1
     print('No' if count_diff >= 2 else 'Yes')
+# (3) の条件、T の方が長い場合
 elif len(S) < len(T):
     for i in range(len(S)):
         if S[i] != T[i]:
@@ -21,6 +71,7 @@ elif len(S) < len(T):
             print('No')
             sys.exit()
     print('Yes')
+# (3) の条件、S の方が長い場合
 else:
     for i in range(len(T)):
         if S[i] != T[i]:
@@ -30,3 +81,4 @@ else:
             print('No')
             sys.exit()
     print('Yes')
+'''
