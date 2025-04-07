a8a9e41a93d416d3f3d66ae4d3c2340ce862f2bc
YotaOkuda <yotaai200@gmail.com>
2025-04-07 14:51:51 +0900

    add UnionFind

--- アルゴリズム/union-find.py
+++ アルゴリズム/union-find.py
@@ -77,3 +77,40 @@ for i in range(M):
         union.unite(u, v)
 
 print(ans)
+
+# OWN2
+N, M = map(int, input().split())
+uv = [list(map(int, input().split())) for _ in range(M)]
+
+class UnionFind:
+    def __init__(self, N):
+        self.par = list(range(N))
+        
+    def root(self, x):
+        if self.par[x] == x:
+            return x
+        else:
+            self.par[x] = self.root(self.par[x])
+            return self.par[x]
+    
+    def same(self, x, y):
+        return self.root(x) == self.root(y)
+    
+    def unite(self, x, y):
+        x = self.root(x)
+        y = self.root(y)
+        if x == y:
+            return
+        self.par[x] = y
+
+ans = 0
+union = UnionFind(N)
+for i in range(M):
+    x = uv[i][0] - 1
+    y = uv[i][1] - 1
+    if union.same(x, y):
+        ans += 1
+    else:
+        union.unite(x, y)
+
+print(ans)
