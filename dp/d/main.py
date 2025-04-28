419d37ad2f838168d6191872ccb834de5810dd73
YotaOkuda <yotaai200@gmail.com>
2025-04-28 22:29:09 +0900

    D(progress)

--- dp/d/main.py
+++ dp/d/main.py
@@ -1,1 +1,24 @@
-test
+N, W = map(int, input().split())
+
+w = []
+v = []
+for i in range(N):
+    goods = list(map(int, input().split()))
+    w.append(goods[0])
+    v.append(goods[1])
+
+dp = [[-1] * (W + 1) for _ in range(N + 1)]
+
+dp[0] = [0] * (W + 1)
+
+ans = 0
+for i in range(N):
+    for j in range(W + 1):
+        if j - w[i] >= 0:
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j - w[i]] + v[i])
+        else:
+            dp[i + 1][j] = max(dp[i + 1][j], dp[i][j])
+    i_max = max(dp[i + 1])
+    ans = max(ans, i_max)
+
+print(ans)
