bb5ab3b6edd61e964f94e58113925191cdaa215c
YotaOkuda <yotaai200@gmail.com>
2025-05-17 11:18:54 +0900

    C, D, E

--- adt_medium_20250515_3/e/main.py
+++ adt_medium_20250515_3/e/main.py
@@ -1,2 +1,26 @@
+'''
+<方針>
+- 先頭から順に見ていく
+- A[i] を正しい場所にスワップする
+  A[A[i]-1] = A[i], A[i] = A[A[i]-1]
+  （例）i=0, A[0] = 2 の場合、A[1] = A[0], A[0] = A[1]（index は0 始まりのため）
+'''
 N = int(input())
 A = list(map(int, input().split()))
+
+count = 0
+ans = []
+i = 0
+while i < N:
+    if A[i] != i + 1:
+        j = A[i] - 1
+        A[i], A[j] = A[j], A[i]
+        ans.append((i + 1, j + 1))
+    else:
+        i += 1
+
+print(len(ans))
+for i, j in ans:
+    print(i, j)
+
+# 計算量 O(N)
